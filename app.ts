45b2f9c8f3da28077b177e415c391265ff201f5b
Tobias <70690455+Tobias@users.noreply.github.com>
2023-01-26 13:49:28 +0100

    Setup
    configured typescript

--- app.ts
+++ app.ts
@@ -1,11 +1,8 @@
-
-
-
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res)=>{
+app.get("/", (req: any, res: any)=>{
     res.send("<h1>Hello world</h1>")
 })
 
