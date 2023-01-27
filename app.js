0935befdeb1d1855bbd6385bae9735eb8e2a99b2
Tobias <70690455+Tobias@users.noreply.github.com>
2023-01-27 19:58:45 +0100

    fddf
    dfd

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-require('node-jsx').install();
-var express = require("express");
+// require('node-jsx').install();
+ var express = require("express");
 var app = express();
-var port = 3000;
-var renderer = require("react-engine");
-var engine = renderer.server.create();
-// 
-app.engine('.jsx', engine);
+const cors = require("cors");
+var port = 5000;
+// var renderer = require("react-engine");
+// var engine = renderer.server.create();
+app.use(cors())
+app.use(express.json());
+// app.engine('.jsx', engine);
 app.set('views', __dirname + '/public/views');
-app.set('view engine', 'jsx');
-app.set("view", renderer.expressView);
+// app.set('view engine', 'jsx');
+// app.set("view", renderer.expressView);
 app.use(express.static(__dirname + "/public"));
-app.get("/", function (req, res) {
-    res.render("page");
+
+app.get("/message", function (req, res) {
+    res.json({message:"Hello from server!" });
 });
 app.listen(port, function () {
     console.log("Server started on port ".concat(port));
--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@
 require('node-jsx').install();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 const renderer = require("react-engine")
 const engine = renderer.server.create();
 
@@ -17,10 +17,14 @@ app.set("view", renderer.expressView)
 
 app.use(express.static(__dirname + "/public"))
 
-app.get("/", (req: any, res: any)=>{
-    res.render("page")
-})
+// app.get("/", (req: any, res: any)=>{
+//     res.render("page")
+// })
 
+app.get("/api", (req:any, res:any)=>{
+    res.json({"users:":["p", "n", "l"]})
+
+})
 
 app.listen(port, ()=>{
 console.log(`Server started on port ${port}`)
