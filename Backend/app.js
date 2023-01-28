1e67df10a30b0231f1149ad5f7ab3f2136554183
Tobias <70690455+Tobias@users.noreply.github.com>
2023-01-28 19:43:26 +0100

    fgf
    fg

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,10 @@
 var express = require("express");
+const workoutRoutes = require('./routes/workout')
 var app = express();
-var port = 3000;
-app.get("/", function (req, res) {
-    res.send("<h1>Hello world</h1>");
-});
+var port = 5000;
+
+app.use('/api/workouts/', workoutRoutes)
+
 app.listen(port, function () {
     console.log("Server started on port ".concat(port));
 });
--- Backend/app.ts
+++ Backend/app.ts
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req: any, res: any)=>{
-    res.send("<h1>Hello world</h1>")
+    res.json({msg: "Hello world"})
 })
 
 
 app.listen(port, ()=>{
 console.log(`Server started on port ${port}`)
 })
+
