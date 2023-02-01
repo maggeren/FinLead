f1eef4ccde5da95f260163d3b95a00384f6b5b82
maggeren <magnusgeert@gmail.com>
2023-02-01 11:26:41 +0100

    config

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import{BrowserRouter, Router, Route, Routes} from "react-router-dom"
-import './App.css';
-import Home from "./pages/Home"
-import Navbar from './components/Navbar';
+import logo from "./logo.svg";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <div className="App">
-     <BrowserRouter>
-    <Navbar/>
-      <div className='pages'>
-        <Routes>
-          <Route
-          path='/'
-            element={<Home />}
-          />
-          
-        </Routes>
-      </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require("express");
 var app = express();
 var port = 3000;
 app.get("/", function (req, res) {
-    res.send("<h1>Hello world</h1>");
+  res.send("<h1>Hello world</h1>");
 });
 app.listen(port, function () {
-    console.log("Server started on port ".concat(port));
+  console.log("Server started on port ".concat(port));
 });
