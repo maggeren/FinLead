f6246c5cf048a6b9bf325f8818a19aa8a5e5fee1
maggeren <magnusgeert@gmail.com>
2023-02-14 15:34:53 +0100

    Mern template

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,12 +1,9 @@
 import express from "express";
 //import getUser from "../controllers/loginController";
 const loginRouter = express.Router();
-import loginUser from "../controllers/loginController";
-
+import { loginUser } from "../controllers/loginController.js";
 loginRouter.post("/api/login", async (req, res) => {
-  console.log("login route");
-  const user = loginUser(req, res);
-  return user ? res.send(200) : res.send(401);
+  await loginUser(req, res);
 });
 
 export default loginRouter;
