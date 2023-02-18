35e72777543daa1ec75dde1077b56c9746eed57d
Tobias <70690455+Tobias@users.noreply.github.com>
2023-02-18 16:15:38 +0100

    bb
    bv

--- frontend/src/components/InputField.js
+++ frontend/src/components/InputField.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { useState } from "react";
+import InlineImage from "./InlineImage";
 
 function InputField(props){
 
-    const manageInput = () => {const[inputType, setInputType] = useState([]);
+    const manageInput = () => {const[inputType, setInputType] = useState(props.type);
 
     }
+
+    const handleClick =(e)=>{
+        inputType === "password" ? setInputType("text"): setInputType("password")
+    }
+
     return(
         <div>
-        <input type={props.text} placeholder={props.placeholder}/>
+        <input type={props.type} placeholder={props.placeholder}/>
+        {props.type === "password" &&(
+            <InlineImage class = "eye-closed" name="eye" clickEvent = {handleClick}/>
+            )}
         <br></br>
+       
         </div>
+        
     )
 
     
