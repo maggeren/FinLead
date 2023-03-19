25a6e1fa9208668a212593975e70181c1cea50f7
maggeren <magnusgeert@gmail.com>
2023-03-19 10:52:47 +0100

    navbar nu med child rendering under baren

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -36,75 +36,77 @@ export const MyNavbar = () => {
   const [filteredSuggestions, setFilteredSuggestions] = useState([]);
 
   return (
-    <Navbar className="myNavbar">
-      <Container fluid style={{ backgroundColor: "transparent" }}>
-        <Navbar.Brand href="#">
-          <img src="/Title.png" className="nav-logo" />
-        </Navbar.Brand>
-        <NavDropdown
-          id="navbarScrollingDropdown"
-          title={<FontAwesomeIcon icon={faBars} />}
-        >
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Form className=" search-bar form-search form-inline">
-          <Form.Control
-            type="search"
-            placeholder="Ticker"
-            className="me-2 search-query"
-            aria-label="Search"
-            onChange={(e) => {
-              const value = e.target.value;
-              setInputValue(value);
-              const filtered = suggestions.filter((suggestion) =>
-                suggestion.toLowerCase().includes(value.toLowerCase())
-              );
-              setFilteredSuggestions(filtered);
-            }}
-          />
-
-          <Link to={"stock/appl"}>
-            <h1>hello</h1>
-          </Link>
-        </Form>
-
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0 link-container"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
+    <div>
+      <Navbar className="myNavbar">
+        <Container fluid style={{ backgroundColor: "transparent" }}>
+          <Navbar.Brand href="#">
+            <img src="/Title.png" className="nav-logo" />
+          </Navbar.Brand>
+          <NavDropdown
+            id="navbarScrollingDropdown"
+            title={<FontAwesomeIcon icon={faBars} />}
           >
-            <Nav.Link href="#action1" className="navBar-link">
-              Rankings
-            </Nav.Link>
-            <Nav.Link href="#action2" className="navBar-link">
-              Forum
-            </Nav.Link>
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">
+              Something else here
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Form className=" search-bar form-search form-inline">
+            <Form.Control
+              type="search"
+              placeholder="Ticker"
+              className="me-2 search-query"
+              aria-label="Search"
+              onChange={(e) => {
+                const value = e.target.value;
+                setInputValue(value);
+                const filtered = suggestions.filter((suggestion) =>
+                  suggestion.toLowerCase().includes(value.toLowerCase())
+                );
+                setFilteredSuggestions(filtered);
+              }}
+            />
 
-            <Nav.Link href="#" className="navBar-link">
-              News
+            <Link to={"stock/appl"}>
+              <h1>hello</h1>
+            </Link>
+          </Form>
+
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0 link-container"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1" className="navBar-link">
+                Rankings
+              </Nav.Link>
+              <Nav.Link href="#action2" className="navBar-link">
+                Forum
+              </Nav.Link>
+
+              <Nav.Link href="#" className="navBar-link">
+                News
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <div className="icon-container">
+            <Nav.Link className="nav-icon">
+              <FontAwesomeIcon icon={faCircleInfo} />
             </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <div className="icon-container">
-          <Nav.Link className="nav-icon">
-            <FontAwesomeIcon icon={faCircleInfo} />
-          </Nav.Link>
-          <Nav.Link className="nav-icon">
-            <FontAwesomeIcon icon={faComment} />
-          </Nav.Link>
-          <Nav.Link className="nav-icon">
-            <FontAwesomeIcon icon={faUserCircle} />
-          </Nav.Link>
-        </div>
-      </Container>
+            <Nav.Link className="nav-icon">
+              <FontAwesomeIcon icon={faComment} />
+            </Nav.Link>
+            <Nav.Link className="nav-icon">
+              <FontAwesomeIcon icon={faUserCircle} />
+            </Nav.Link>
+          </div>
+        </Container>
+      </Navbar>
       <Outlet></Outlet>
-    </Navbar>
+    </div>
   );
 };
