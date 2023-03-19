a7417d863a3da815991c1635846cea9f6fd8e63c
Tobias <70690455+Tobias@users.noreply.github.com>
2023-03-19 12:35:37 +0100

    fixed login styling

--- frontend/src/components/InlineImage.js
+++ frontend/src/components/InlineImage.js
@@ -2,14 +2,8 @@ import React, { useState } from "react";
 
 
 function InlineImage(props){
-
-     const customStyle ={
-        marginLeft: "146px",
-        marginBottom: "-20px",
-        cursor: "pointer"
-     };
      
-    return <span class="ab"><i className={props.class} id={props.id} name={props.name} onClick ={props.clickEvent} style={customStyle}>{props.image}</i></span>
+    return <i className={props.className} id={props.id} name={props.name} onClick={props.clickEvent} >{props.image}</i>
 }
 
 export default InlineImage;
