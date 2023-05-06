b0cbc1fa6db9dfc7e7d26b8d8dfd3139d4e1a9f9
mikkeld304 <92784234+mikkeld304@users.noreply.github.com>
2023-05-06 13:35:17 +0200

    Searchbar
    Ændringer til searchbar

--- Backend/server.js
+++ Backend/server.js
@@ -2,34 +2,32 @@ import express from "express";
 import session from "express-session";
 import cors from "cors";
 import "dotenv/config";
-import redis from "redis";
+//import redis from "redis";
 import { routes } from "./routes/exports.js";
 import connectDB from "./config/db.js";
-const redisClient = redis.createClient();
-redisClient.connect();
+//const redisClient = redis.createClient();
+//redisClient.connect();
 const PORT = process.env.PORT;
 connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200,
-  credentials: true,
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    credentials: true,
 };
 app.use(cors(corsOptions));
-app.use(
-  session({
+app.use(session({
     secret: "mysecret",
     resave: false,
     saveUninitialized: true,
     cookie: {
-      maxAge: 360000,
-      sameSite: true,
-      secure: true, // set to true if using HTTPS
+        maxAge: 360000,
+        sameSite: true,
+        secure: true, // set to true if using HTTPS
     },
-  })
-);
+}));
 //_________ROUTES_________
 app.all("/api/register", routes.registerRouter);
 app.all("/api/login", routes.loginRouter);
@@ -38,7 +36,5 @@ app.all("/api/checkLogin", routes.loginRouter);
 app.all("/api/comment", routes.authenticateRouter);
 app.all("/api/searchbar", routes.searchBarRouter);
 //________________________
-const server = app.listen(PORT, () =>
-  console.log(`💻 Server  started on http://localhost:${PORT} 💻`)
-);
-export default redisClient;
+const server = app.listen(PORT, () => console.log(`💻 Server  started on http://localhost:${PORT} 💻`));
+//export default redisClient;
--- backend/controllers/stock/searchBarController.js
+++ backend/controllers/stock/searchBarController.js
@@ -7,28 +7,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import redisClient from "../../server.js";
+//import redisClient from "../../server.js";
 import Ticker from "../../models/Ticker.js";
 function getStockDataFromDB(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        const ticker = yield redisClient.get("tickers");
-        if (ticker) {
-            res.status(200).json(JSON.parse(ticker)); //send the tickers to the frontend
-        }
-        else {
-            const tickers = yield Ticker.find();
-            const tickerArray = tickers.map((ticker) => {
-                return {
-                    ticker: ticker.ticker,
-                    companyName: ticker.companyName,
-                    sector: ticker.sector,
-                };
-            });
-            yield redisClient.set("tickers", JSON.stringify(tickerArray), {
-                EX: 21600, // let key expire after 6 hours
-            });
-            res.status(200).json(tickerArray);
-        }
+        //const ticker = await redisClient.get("tickers");
+        //if (ticker) {
+        //res.status(200).json(JSON.parse(ticker)); //send the tickers to the frontend
+        //} else {
+        const tickers = yield Ticker.find();
+        const tickerArray = tickers.map((ticker) => {
+            return {
+                ticker: ticker.ticker,
+                companyName: ticker.companyName,
+                sector: ticker.sector,
+            };
+        });
+        //await redisClient.set("tickers", JSON.stringify(tickerArray), {
+        //EX: 21600, // let key expire after 6 hours
+        //});
+        res.status(200).json(tickerArray);
+        //}
     });
 }
 export const searchBarController = { getStockDataFromDB };
